1b739c8abbd99f19d9c5f27c88aa0b3560b2f80a
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-02-24 23:17:58 +0200

    виконав друге завдання

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// отримуємо доступ до нашого 'ul' в розмітці
+const ingredientsEl = document.querySelector("#ingredients");
+
+// перебираємо масив з назвами інгридієнтів і для кожного додажємо клас та контент
+ingredients.forEach((ingredient) => {
+  const ingredientEl = document.createElement("li");
+
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add("item");
+
+  // добавляємо створену різмітку до DOM
+  ingredientsEl.appendChild(ingredientEl);
+});
