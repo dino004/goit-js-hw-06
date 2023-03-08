94ad134c438433d266a62c9ac4ed750bc459d75b
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-03-08 22:37:53 +0200

    виконав шосте завдання

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("#validation-input");
+const inputData = document.querySelector('[data-length="6"]');
+
+input.addEventListener("blur", onInputChange);
+
+function onInputChange(event) {
+  if (event.currentTarget.selectionEnd >= inputData.dataset.length) {
+    this.classList.remove("invalid");
+    this.classList.add("valid");
+  } else {
+    this.classList.remove("valid");
+    this.classList.add("invalid");
+  }
+}
