7d3ff741d9636dc8b78e51930c355e21096448b5
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-03-11 16:38:39 +0200

    виконав дев'яте завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+const inputs = document.querySelectorAll("input");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  inputs.forEach((valueField) => {
+    if (valueField.value === "") {
+      alert("Заповніть, будь ласка, всі поля!");
+    } else {
+      let obj = {};
+      obj.email = event.currentTarget.elements.email.value;
+      obj.password = event.currentTarget.elements.password.value;
+      console.log(obj);
+    }
+  });
+  form.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  body: document.querySelector("body"),
+  btnGeneretionColor: document.querySelector(".change-color"),
+  outNameColor: document.querySelector(".color"),
+};
+
+refs.btnGeneretionColor.addEventListener("click", getColor);
+
+function getColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.outNameColor.textContent = getRandomHexColor();
+}
